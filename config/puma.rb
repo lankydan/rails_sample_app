6d4c59c0d9b4abd5f986b9bf6798426fe58f22ec
Dan Newton <danknewton@hotmail.com>
2017-05-03 06:34:58 +0100

    correct puma file

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,18 @@
 # commented out code below to run on windows, due to puma not running on windows
 
-# workers Integer(ENV["WEB_CONCURRENCY"] || 2)
-# threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5)
-# threads threads_count, threads_count
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5)
+threads threads_count, threads_count
 
-# preload_app!
+preload_app!
 
-# rackup DefaultRackup
-# port ENV["PORT"] || 3000
-# environment ENV["RACK_ENV"] || "development"
+rackup DefaultRackup
+port ENV["PORT"] || 3000
+environment ENV["RACK_ENV"] || "development"
 
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection
-# end
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 
 # ------------------------------------------
 
