a25370846e940bd2ee5304581971d746e72c3d56
Dan Newton <danknewton@hotmail.com>
2017-05-21 16:37:39 +0100

    fix heroku by removing image uploading for config files?

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,11 +2,17 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
+  # include CarrierWave::MiniMagick
+  # process resize_to_limit: [400, 400]
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
+
   if Rails.env.production?
-    storage :fog
-  else
+    include CarrierWave::MiniMagick
+    process resize_to_limit: [400, 400]
     storage :file
   end
 
